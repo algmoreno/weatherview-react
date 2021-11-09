f1d92afd08cea616431f746fc0211348823e2779
algmoreno <alg.moreno00@gmail.com>
2021-11-09 12:51:26 -0700

    prelim display card

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 
 function Display() {
-  return(
-    "Display"
+  return (
+    <div className='card-container'>
+      <div className='row justify-content-start'>
+        <div className='col-8 card'>
+          <Card style={{ width: '48rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Display; 
+export default Display;
