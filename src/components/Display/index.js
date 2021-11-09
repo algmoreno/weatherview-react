22e284b1b31f44ea1d777e574928f2180daf74d6
algmoreno <alg.moreno00@gmail.com>
2021-11-09 13:03:18 -0700

    display card sections

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -12,20 +12,21 @@ function Display() {
           <Card style={{ width: '48rem' }}>
             <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Date</Card.Title>
+              <h2>City</h2>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              <ListGroupItem>Currently: </ListGroupItem>
+              <ListGroupItem>Humidity:</ListGroupItem>
+              <ListGroupItem>UV Index:</ListGroupItem>
             </ListGroup>
             <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Card.Link href="#">10-Day Forecast</Card.Link>
+              <Card.Link href="#">Add to Favorites</Card.Link>
             </Card.Body>
           </Card>
         </div>
