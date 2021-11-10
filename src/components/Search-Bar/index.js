6ff312d251f12b13334381560db9f8712d8f73f3
algmoreno <alg.moreno00@gmail.com>
2021-11-09 19:08:14 -0700

    function for api fetch call

--- src/components/Search-Bar/index.js
+++ src/components/Search-Bar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -8,20 +9,36 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 
-function searchBar() {
+const SearchBar = () => {
+
+  const [city, setCity] = useState('')
+
+  fetchWeather();
+
+  function fetchWeather(city) {
+    fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=c3db145bc89912e27b13b4d5a94e0f9d`)
+    .then(function(response) {
+        response.json().then(function(data){
+          console.log(data)
+        })
+      })
+  }
+
   return (
     <Navbar className='search-main' expand="lg">
       <Container className='row justify-content-start search-container' fluid>
         <div className='col-9'>
         </div>
         <div className='col-3'>
-          <Form className="d-flex">
+          <Form className="d-flex"> 
             <FormControl
+              onChange={event => setCity(event.target.value)}
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search" />
-            <Button variant="outline-success">Search</Button>
+            <Button 
+             variant="outline-success">Search</Button>
           </Form>
         </div>
       </Container>
@@ -29,4 +46,4 @@ function searchBar() {
   )
 }
 
-export default searchBar;
+export default SearchBar;
